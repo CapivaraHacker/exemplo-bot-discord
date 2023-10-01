d5a7da93cc90e84ec966598ec8c8c9cc9f7eb372
Capivara Hacker <122393242+CapivaraHacker@users.noreply.github.com>
2023-09-30 21:02:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import discord
 from discord.ext import commands
 
 # Intents e criação do bot
-intents = discord.Intents.all()
+intents = discord.Intents.all() # Ativa todas as intents
 bot = commands.Bot('!', intents=intents)
 
 
 # Comando "!olá"
-
 @bot.command(aliases=["echo", "oi"])
-async def olá(ctx):
-    autor = ctx.author.mention
-    await ctx.send(f'Olá, {autor}!')
+async def olá(ctx): # Cria a função assíncrona
+    autor = ctx.author.mention # Menção do autor
+    await ctx.send(f'Olá, {autor}!') # Envia uma mensagem
 
-bot.run('MTE1NzgxMzA1NDI2MDY1MDAwNA.GIAtQw.GUucsVmaMHgCNw7quMx683vlxBFnsw6Y7Eu6zU')
+bot.run('MTE1NzgxMzA1NDI2MDY1MDAwNA.GIAtQw.GUucsVmaMHgCNw7quMx683vlxBFnsw6Y7Eu6zU') # Roda o bot
